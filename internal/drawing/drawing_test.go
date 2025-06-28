2f7473d44ecbbf57e2c3ae52c20b8861cef707af
Emrick Sorensen <emrickishere@gmail.com>
2025-06-28 16:21:27 -0600

    Added calculations of angles and endpoints from position

--- internal/drawing/drawing_test.go
+++ internal/drawing/drawing_test.go
@@ -1,6 +1,9 @@
 package drawing
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestAdd(t *testing.T) {
 	result := add(2, 3)
@@ -27,5 +30,30 @@ func TestAddWithTable(t *testing.T) {
 		if result != tt.expected {
 			t.Errorf("add(%d, %d) = %d; want %d", tt.a, tt.b, result, tt.expected)
 		}
+	}
+}
+
+func TestPosToRad(t *testing.T) {
+	result := PosToRad(204, 816)
+	expected := math.Pi / 2
+	if result != expected {
+		t.Errorf("PosToRad(204, 816) = %d; want %d", result, expected)
+	}
+}
+
+func TestFindEndpoints(t *testing.T) {
+
+	x0, y0, x1, y1 := FindEndpoints(100, math.Pi/3)
+	if x0 != uint(math.Round(50*(math.Sqrt(3)/2+1))) {
+		t.Errorf("x0 = %d; want 93", x0)
+	}
+	if y0 != 75 {
+		t.Errorf("y0 = %d; want 75", y0)
+	}
+	if x1 != uint(math.Round(100-50*(math.Sqrt(3)/2+1))) {
+		t.Errorf("x1 = %d; want 7", x1)
+	}
+	if y1 != 25 {
+		t.Errorf("y1 = %d; want 25", y1)
 	}
 }
